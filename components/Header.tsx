411fcdefd1daf90872d410f91918760817f93c37
Metarock <esang037@gmail.com>
2022-06-01 22:07:26 +1200

    setup header component; added heroicon and reacticons

--- components/Header.tsx
+++ components/Header.tsx
@@ -3,7 +3,7 @@ const Header = () => {
   return (
     <header>
       {/* Left */}
-      <div>
+      <div className="flex items-center space-x-2 md:space-x-10">
         {/* svg elements */}
         <img
           src="https://rb.gy/ulxxee"
@@ -12,10 +12,16 @@ const Header = () => {
           className="cursor-pointer object-contain"
         />
 
-        <ul></ul>
+        <ul className="hidden space-x-4 md:flex">
+          <li className="headerLink">Home</li>
+          <li className="headerLink">TV Shows</li>
+          <li className="headerLink">Movies</li>
+          <li className="headerLink">New & Popular</li>
+          <li className="headerLink">My List</li>
+        </ul>
       </div>
       {/* Right */}
-      <div></div>
+      <div className=""></div>
     </header>
   )
 }
