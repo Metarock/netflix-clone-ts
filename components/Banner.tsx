d3ac2f322a38f6017608b47448afd3a37d1f2910
Metarock <esang037@gmail.com>
2022-06-04 13:23:57 +1200

    added type definitions

--- components/Banner.tsx
+++ components/Banner.tsx
@@ -1,7 +1,35 @@
-import React from 'react'
+import { useEffect, useState } from 'react'
+import { Movie } from '../typings'
+import Image from 'next/image'
+import { BASE_URL } from '../constants/movie'
 
-const Banner = () => {
-  return <div>Banner</div>
+interface BannerProps {
+  netflixOriginals: Movie[]
+}
+
+const Banner = ({ netflixOriginals }: BannerProps) => {
+  const [movie, setMovie] = useState<Movie | null>(null)
+
+  // Get a random movie on banner by using the default math
+  // floor and random via the netflix original length
+  useEffect(() => {
+    setMovie(
+      netflixOriginals[Math.floor(Math.random() * netflixOriginals.length)]
+    )
+  }, [netflixOriginals])
+
+  console.log('movie ', movie)
+  return (
+    <div>
+      <div className="absolute top-0 left-0">
+        {/* image nextjs compnent */}
+        <Image
+          src={`${BASE_URL}${movie?.backdrop_path || movie?.poster_path}`}
+          layout="fill"
+        />
+      </div>
+    </div>
+  )
 }
 
 export default Banner
---
+++ constants/movie.ts
@@ -0,0 +1,2 @@
+export const API_KEY = process.env.NEXT_PUBLIC_TDMB_APIKEY
+export const BASE_URL = process.env.NEXT_PUBLIC_BASE_URL
